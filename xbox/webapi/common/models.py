24f3721e50aab22b6172a160527ea7df428721b7
nthumann <me@n-thumann.de>
2023-08-08 15:30:57 +0200

    Rename config setting due to Pydantic v2

--- xbox/webapi/common/models.py
+++ xbox/webapi/common/models.py
@@ -18,19 +18,19 @@ def to_lower(string):
 class PascalCaseModel(BaseModel):
     class Config:
         arbitrary_types_allowed = True
-        allow_population_by_field_name = True
+        populate_by_name = True
         alias_generator = to_pascal
 
 
 class CamelCaseModel(BaseModel):
     class Config:
         arbitrary_types_allowed = True
-        allow_population_by_field_name = True
+        populate_by_name = True
         alias_generator = to_camel
 
 
 class LowerCaseModel(BaseModel):
     class Config:
         arbitrary_types_allowed = True
-        allow_population_by_field_name = True
+        populate_by_name = True
         alias_generator = to_lower
